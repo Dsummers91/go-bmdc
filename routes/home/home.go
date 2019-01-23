720bb775383689f8a25b054fb9062927e83d667d
Deon <deonsummers91@gmail.com>
2019-01-23 08:29:11 -0600

    add variables to routes

--- routes/home/home.go
+++ routes/home/home.go
@@ -15,11 +15,13 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		Auth0ClientSecret string
 		Auth0Domain       string
 		Auth0CallbackURL  template.URL
+		IsLoggedIn        bool
 	}{
 		os.Getenv("AUTH0_CLIENT_ID"),
 		os.Getenv("AUTH0_CLIENT_SECRET"),
 		os.Getenv("AUTH0_DOMAIN"),
 		template.URL(os.Getenv("AUTH0_CALLBACK_URL")),
+		false,
 	}
 
 	templates.RenderTemplate(w, r, "home", data)
--- routes/templates/templates.go
+++ routes/templates/templates.go
@@ -5,7 +5,18 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"github.com/dsummers91/go-bmdc/app"
 )
+
+type TemplateData struct {
+	Auth0ClientId     string
+	Auth0ClientSecret string
+	Auth0Domain       string
+	Auth0CallbackURL  template.URL
+	Profile           interface{}
+	IsLoggedIn        bool
+}
 
 func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, data interface{}) {
 	cwd, _ := os.Getwd()
@@ -21,20 +32,23 @@ func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, data in
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	//session, err := app.Store.Get(r, "auth-session")
-	//if err != nil {
-	//		http.Error(w, err.Error(), http.StatusInternalServerError)
-	//	return
-	//}
-
-	//fmt.Println(data)
-	//if _, ok := session.Values["profile"]; !ok {
-	//data["profile"] = session.Values["profile"]
-	//data["isLoggedIn"] = true
-	//}
+	session, err := app.Store.Get(r, "auth-session")
+	if err == nil {
+		if profile, ok := session.Values["profile"]; ok {
+			data = TemplateData{
+				Profile:           profile,
+				IsLoggedIn:        true,
+				Auth0Domain:       os.Getenv("AUTH0_DOMAIN"),
+				Auth0CallbackURL:  template.URL(os.Getenv("AUTH0_CALLBACK_URL")),
+				Auth0ClientId:     os.Getenv("AUTH0_CLIENT_ID"),
+				Auth0ClientSecret: os.Getenv("AUTH0_CLIENT_SECRET"),
+			}
+		}
+	}
 
 	err = t.Execute(w, data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
