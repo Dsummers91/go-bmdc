f2a59a4137ff01e5857654d7bbca92f7dd2ec81b
Deon <deonsummers91@gmail.com>
2019-01-24 16:43:13 -0600

    more user settings

--- public/user.js
+++ public/user.js
@@ -3,3 +3,22 @@ $(document).ready(function() {
       Cookies.remove('auth-session');
     });
 });
+
+
+$("#editProfile").submit(function( event ) {
+  event.preventDefault();
+  console.log(event)
+  $.post( "/profile", { city: "blahd"})
+  .done(function( data ) {
+    var $inputs = $('#editProfile :input');
+    console.log($inputs);
+
+    // not sure if you wanted this, but I thought I'd add it.
+    // get an associative array of just the values.
+    var values = {};
+    $inputs.each(function() {
+      values[this.name] = $(this).val();
+    });
+    alert( "Data Loaded: " + data );
+  });
+});
