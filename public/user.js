ec8f27cb0ce78d0af3422f980ad3c4a75c0f6f34
Deon <deonsummers91@gmail.com>
2019-02-24 20:06:47 -0600

    update

--- public/user.js
+++ public/user.js
@@ -25,7 +25,27 @@ $("#editProfile").submit(function( event ) {
     dataType: 'json',
     async: false,
     success: function( data ) {
-      console.log($inputs);
+      alert( "Data Loaded: " + data );
+    }
+  });
+})
+
+$("#submitContact").submit(function( event ) {
+  event.preventDefault();
+  var $inputs = $('#submitContact :input');
+  var values = {};
+  $inputs.each(function() {
+    values[this.name] = $(this).val();
+  });
+
+  $.ajax({
+    url: 'http://localhost:3000/contact',
+    type: 'POST',
+    data: JSON.stringify(values),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    async: false,
+    success: function( data ) {
       alert( "Data Loaded: " + data );
     }
   });
